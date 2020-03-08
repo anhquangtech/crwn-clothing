056993c9a05d15e353b7880de423da5c50e0f2e1
quangda <quangda@solazu.com>
2020-03-08 13:32:38 +0700

    [master]: Complete Sign up with email and password

--- src/pages/signIn-signUp/signInsignUp.component.jsx
+++ src/pages/signIn-signUp/signInsignUp.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './signInsignUp.styles.scss';
 
 import SignIn from '../../components/sign-in/signIn.component';
+import SignUp from '../../components/sign-up/signUp.component';
 
 const signInSignUpPage = () => (
     <div className="sign-in-and-sign-up">
         <SignIn />
+        <SignUp />
     </div>
 )
 
