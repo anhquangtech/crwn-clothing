b2599c3ec18e70771a855a72203842585331f418
quangda <quangda@solazu.com>
2020-04-19 10:56:38 +0700

    move directory and shop data into reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,33 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react'
+import CollectionOverview from '../../components/collection-overview/collection-overview.component'
+import { Route } from 'react-router-dom'
+import CollectionPage from '../collection/collection.component'
 
-import SHOP_DATA from './shop.data';
-import PreviewCollection from '../../components/preview-collection/preview-collection.component';
+const Shop = ({ match }) => (
+	<div className='shop-page'>
+		<Route exact path={`${match.path}`} component={CollectionOverview} />
+		<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+	</div>
+)
 
-class Shop extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <PreviewCollection
-                          key={id}
-                          {...otherCollectionProps}
-                        />
-                    ))
-                }
-            </div>
-        );
-    }
-}
-
-export default Shop;
+export default Shop
